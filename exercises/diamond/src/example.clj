8ad10a3c5dfef2edf46beb296fce65a847b50de3
Michael Fogleman <michaelwfogleman@gmail.com>
2017-06-12 08:22:23 -0400

    Initial solution.

--- exercises/diamond/src/example.clj
+++ exercises/diamond/src/example.clj
@@ -1,3 +1,38 @@
-(ns diamond)
+(ns diamond
+  (:require [clojure.string :as str]))
 
-(defn diamond [])
+(def capitals (vec "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+
+(defn ordinal [c]
+  (str/index-of capitals c))
+
+(defn spaces [n]
+  (str/join (repeat n " ")))
+
+(defn middle [o]
+  (let [c (capitals o)]
+    (if (zero? o)
+      (str c)
+      (str c (spaces (- (* 2 o) 1)) c))))
+
+(defn line [m o]
+  (str (spaces (- m o)) (middle o)))
+
+(defn top-half [m]
+  (map (partial line m) (range m)))
+
+(defn diamond-lines [c]
+  (let [m (ordinal c)
+        top (top-half m)]
+    (concat top [(middle m)] (reverse top))))
+
+(defn diamond-str [c]
+  (str/join "\n" (diamond-lines c)))
+
+(defn diamond [input]
+  (let [c (first input)]
+    (diamond-str c)))
+
+
+
+
